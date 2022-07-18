74d339e4f5346f204b3f4fbc9ac89dd99f979c44
sundaymi <sundaymii21@gmail.com>
2022-07-18 18:03:51 +0300

    changes in db part

--- pharmacystore/settings.py
+++ pharmacystore/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os , dotenv 
+import os, dotenv 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
