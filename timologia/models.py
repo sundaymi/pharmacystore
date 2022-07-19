6a44dc0b6b76fa54958dd73519f141982a7c39d4
sundaymi <sundaymii21@gmail.com>
2022-07-19 13:05:27 +0300

    changes in models

--- timologia/models.py
+++ timologia/models.py
@@ -1,11 +1,24 @@
+from tkinter import CASCADE
 from django.db import models
+from .models import Pharmasists
 
 # Create your models here.
 
-class Timologio(models.Model):
+class Timologio_Pharmasist_SaleMen(models.Model):
     t_name = models.CharField(max_length=100)
     manufactured = models.CharField(max_length=100)
     cost = models.DecimalField(max_digits=10, decimal_places=10)
     quantity = models.IntegerField()
     total_cost = models.DecimalField(max_digits=10, decimal_places=10)
+    date_order = models.DateTimeField()
+    p_id = models.ForeignKey(Pharmasists, on_delete=models.CASCADE)
+    
+
+class Apodeiksi_Pharmasist_User(models.Model):
+    a_name = models.CharField(max_length=100)
+    p_id = models.ForeignKey(Pharmasists, on_delete=models.CASCADE)
+    d_name = models.ForeignKey()
+    quantity = models.IntegerField()
+    cost = models.DecimalField(max_digits=10, decimal_places=10)
+    date_order = models.DateTimeField() 
     
--- users/models.py
+++ users/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from .models import Drug
+
 
 # Create your models here.
 
@@ -12,10 +14,28 @@ class User(models.Model):
     location = models.CharField(max_length=80)
     city = models.CharField(max_length=80)
     tk = models.IntegerField()
-    phone = models.IntegerField()
+    phone = models.IntegerField() 
     
     
-class Pharmasists(models.Model):
+    def __str__(self):
+        return self.first_name
+    
+   
+class SaleMen(models.Model):
+    fname = models.CharField(max_length=70)
+    lname = models.CharField(max_length=70)
+    sm_email = models.EmailField(unique=True)
+    sm_pass = models.IntegerField()
+    sm_storename = models.CharField(max_length=80) 
+    sm_address = models.CharField(max_length=70) 
+    sm_location = models.CharField(max_length=70) 
+    sm_city = models.CharField(max_length=70) 
+    sm_tk = models.IntegerField()
+    sm_phone= models.IntegerField() 
+    sm_afm = models.IntegerField() 
+    
+    
+class Pharmasist(models.Model):
     f_name = models.CharField(max_length=70)
     l_name = models.CharField(max_length=70)
     p_email = models.EmailField(unique=True)
@@ -27,14 +47,6 @@ class Pharmasists(models.Model):
     p_city = models.CharField(max_length=70)
     p_tk = models.IntegerField()
     p_phone = models.IntegerField() 
-    #sales_id = 
-    
-    
-class SaleMen(models.Model):
-    fname = models.CharField(max_length=70)
-    lname = models.CharField(max_length=70)
-    sm_email = models.EmailField(unique=True)
-    sm_pass = models.IntegerField()
-    sm_phone= models.IntegerField() 
-    afm = models.IntegerField() 
+    sm_id = models.ForeignKey(SaleMen, on_delete=models.CASCADE)
+    drug_id = models.ManyToManyField(Drug) 
     
