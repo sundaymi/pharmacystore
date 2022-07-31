8717857291f893dae80235ac98841193f9ff30b6
sundaymi <sundaymii21@gmail.com>
2022-07-31 18:18:17 +0300

    label removed

--- users/forms.py
+++ users/forms.py
@@ -13,19 +13,19 @@ class RegisterUserForm(forms.Form):
     
     
 class RegisterPharmasistForm(forms.Form):
-    username = forms.Charfield(label='Username',max_length=100) 
-    password =  forms.CharField(label='Password',widget=forms.PasswordInput(), validators=[validators.MinLengthValidator(8)])
-    confirm_password = forms.CharField(label='Confirm Password',widget=forms.PasswordInput())
-    firstname = forms.Charfield(label='First Name',max_length=100) 
-    lastname = forms.Charfield(label='Last Name',max_length=120)  
-    email = forms.EmailField(label='Email')
+    username = forms.Charfield(max_length=100) 
+    password =  forms.CharField(widget=forms.PasswordInput(), validators=[validators.MinLengthValidator(8)])
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
+    firstname = forms.Charfield(max_length=100) 
+    lastname = forms.Charfield(max_length=120)  
+    email = forms.EmailField()
     phone_number = forms.CharField(label='Phone',required=False)
     
 
 class RegisterSaleMenForm(forms.Form):
-    username = forms.Charfield(label='Username',max_length=100) 
-    password =  forms.CharField(label='Password',widget=forms.PasswordInput(), validators=[validators.MinLengthValidator(8)])
-    confirm_password = forms.CharField(label='Confirm Password',widget=forms.PasswordInput())
+    username = forms.Charfield(max_length=100) 
+    password =  forms.CharField(widget=forms.PasswordInput(), validators=[validators.MinLengthValidator(8)])
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
     firstname = forms.Charfield(max_length=100) 
     lastname = forms.Charfield(max_length=120)  
     email = forms.EmailField()
